67cbf6dfe6911dfcc10369a6bf348a881a876187
Nayany Nobre <102068798+nayanynobre@users.noreply.github.com>
2024-12-10 20:24:24 -0300

    Add files via upload

---
+++ todos/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView
+from .models import Todo
+
+class TodoListView(ListView):
+    model = Todo
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ['title','deadline']
+    success_url = reverse_lazy('todo_list')
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ['title','deadline']
+    success_url = reverse_lazy('todo_list')
